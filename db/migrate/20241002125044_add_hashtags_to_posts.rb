e2436c45c8972a80f75ddcae1daecf7cedbfa1e7
mmmaaaariii <166019982+mmmaaaariii@users.noreply.github.com>
2024-10-13 09:31:33 +0000

    マイグレーション修正

--- db/migrate/20241002125044_add_hashtags_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddHashtagsToPosts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :posts, :hashtags, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_10_072207) do
+ActiveRecord::Schema.define(version: 2024_10_09_234538) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -99,8 +99,6 @@ ActiveRecord::Schema.define(version: 2024_10_10_072207) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "hashtags"
-    t.string "name"
   end
 
   create_table "relationships", force: :cascade do |t|
